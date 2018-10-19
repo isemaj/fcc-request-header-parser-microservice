e8d7be74f6428764d2cac8775db51adaf4f91236
James Itum <jamesitum@protonmail.ch>
2018-10-19 08:02:11 +0800

    change sendfile path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors({optionSuccessStatus: 200}));
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get("/api/whoami", function (req, res) {
